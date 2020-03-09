6aabec1540305e5bbba36dd276ea63be6ee7fae9
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2020-03-09 07:47:53 +0000

    Implemented register and login functionality

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from "./routes/index";
+import * as firebase from "firebase";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const configOptions = {
+  apiKey: "AIzaSyAZkAvEaQC91IMqVRF32-nMOV9YTCKyhW0",
+  authDomain: "vue-firebase-auth-639fe.firebaseapp.com",
+  databaseURL: "https://vue-firebase-auth-639fe.firebaseio.com",
+  projectId: "vue-firebase-auth-639fe",
+  storageBucket: "vue-firebase-auth-639fe.appspot.com",
+  messagingSenderId: "562258247440",
+  appId: "1:562258247440:web:512c558302a7f15442af95"
+};
+
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
